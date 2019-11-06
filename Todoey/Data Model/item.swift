9db04fbb75a4ebb65d519be1ee8c2e6341979f36
Ryosuke Oe <oe@rabbix.net>
2019-11-06 19:59:49 +0900

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by 大江諒介 on 2019/11/06.
+//  Copyright © 2019 rabbix. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
